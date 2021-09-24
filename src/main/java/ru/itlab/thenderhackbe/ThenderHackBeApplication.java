138c827855f5255f96db406037191d1930b92d40
koala1101 <rproba@list.ru>
2021-09-24 18:39:49 +0300

    Initial commit

---
+++ src/main/java/ru/itlab/thenderhackbe/ThenderHackBeApplication.java
@@ -0,0 +1,13 @@
+package ru.itlab.thenderhackbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThenderHackBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThenderHackBeApplication.class, args);
+    }
+
+}
